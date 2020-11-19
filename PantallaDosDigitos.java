dc60661360093da221710aa4bae9e7668644ba9c
beatranche <56152901+beatranche@users.noreply.github.com>
2020-11-19 09:30:27 +0100

    0167.02 Refactorizacion metodo incrementaValorAlmacenado correcto

--- PantallaDosDigitos.java
+++ PantallaDosDigitos.java
@@ -61,7 +61,7 @@ public class PantallaDosDigitos
      * Incrementa el valor de la pantalla. Si llega al limite cambia al valor inicial.
      */
     public void incrementaValorAlmacenado() {
-        valorAlmacenado = ((valorAlmacenado + 1) % (valorLimite - valorInicial)) + valorInicial;
+        valorAlmacenado = (((valorAlmacenado + 1) - valorInicial) % (valorLimite - valorInicial)) + valorInicial;
     }
     
-}
+      }
